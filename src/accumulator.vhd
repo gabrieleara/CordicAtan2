6b3eee9867fa59fda494e7eaa684ba1e039f1568
Gabriele Ara <gabriele.ara@live.it>
2017-06-15 15:09:09 +0200

    Changed basic components
    Since the CORDIC algorithm will require subtractions and different
    accumulators, we changed the basic components of the project.
    Little modification to the MuxWithOffest too, to make it more scalable.

--- src/accumulator.vhd
+++ src/accumulator.vhd
@@ -1,27 +1,29 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity Counter is
+entity Accumulator is
 	generic (size : positive := 8);
 	port (
 		clock	: in	std_ulogic;
 		reset	: in	std_ulogic;
+		zero	: in	std_ulogic; -- synchronous reset of the accumulated value
 		inA		: in	std_ulogic_vector(size-1 downto 0);
+		sumSub	: in	std_ulogic;
 		value	: out	std_ulogic_vector(size-1 downto 0)
 	);
-end Counter;
+end Accumulator;
 
-architecture Counter_Arch of Counter is
-	component RippleCarryAdder
+architecture Accumulator_Arch of Accumulator is
+	component AdderSubtractor is
 		generic (size : positive := 8);
 		port (
 			inA			: in	std_ulogic_vector(size-1 downto 0);
 			inB			: in	std_ulogic_vector(size-1 downto 0);
-			carryIn		: in	std_ulogic;
+			sumSub		: in	std_ulogic;
 			carryOut	: out	std_ulogic;
-			sum			: out	std_ulogic_vector(size-1 downto 0)
+			value		: out	std_ulogic_vector(size-1 downto 0)
 		);
-	end component RippleCarryAdder;
+	end component;
 
 	component GRegister
 		generic (size : positive := 8);
@@ -38,18 +40,19 @@ architecture Counter_Arch of Counter is
 
 	-- Wire used to loopback the output of the register into the input of the
 	-- adder and to connect it to the output of the design
+	signal cvalue	: std_ulogic_vector(size-1 downto 0);
 	signal loopback	: std_ulogic_vector(size-1 downto 0);
 
 begin
 
-	rcaInstance : RippleCarryAdder
+	addsubInstance : AdderSubtractor
 		generic map(size => size)
 		port map (
 			inA			=> inA,
 			inB			=> loopback,
-			carryIn		=> '0',
+			sumSub		=> sumSub,
 			carryOut	=> open,
-			sum			=> data
+			value		=> data
 		);
 
 	registerInstance : GRegister
@@ -58,10 +61,14 @@ begin
 			clock	=> clock,
 			reset	=> reset,
 			data	=> data,
-			value	=> loopback
+			value	=> cvalue
 		);
 
-	-- The output is the value in the loopback wire
-	value <= loopback;
+	-- The output is the output value of the register
+	value <= cvalue;
 
-end Counter_Arch;
+	-- The loopback is the output value of the register, unless the synchronous
+	-- reset "zero" is set to 1
+	loopback <= cvalue when zero == '0' else (others => '0');
+
+end Accumulator_Arch;
